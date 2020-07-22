1c758c6e505587cbc304e2211d19082e75802162
Akshay Ghodake <aghodake@redhat.com>
2020-07-23 00:56:30 +0530

    [Spec-Update] pcs_status spac updated to first_file. (#2674)
    Signed-off-by: Akshay Ghodake <aghodake@redhat.com>
    (cherry picked from commit 82256becd792722ef37c08d559c2d31becfeec80)

--- insights/specs/sos_archive.py
+++ insights/specs/sos_archive.py
@@ -104,7 +104,7 @@ class SosSpecs(Specs):
     ovs_vsctl_show = simple_file("sos_commands/openvswitch/ovs-vsctl_-t_5_show")
     pcs_config = simple_file("sos_commands/pacemaker/pcs_config")
     pcs_quorum_status = simple_file("sos_commands/pacemaker/pcs_quorum_status")
-    pcs_status = simple_file("sos_commands/pacemaker/pcs_status")
+    pcs_status = first_file(["sos_commands/pacemaker/pcs_status", "/sos_commands/pacemaker/pcs_status_--full"])
     podman_image_inspect = glob_file("sos_commands/podman/podman_inspect_*")
     podman_list_containers = first_file(["sos_commands/podman/podman_ps_-a", "sos_commands/podman/podman_ps"])
     podman_list_images = simple_file("sos_commands/podman/podman_images")
